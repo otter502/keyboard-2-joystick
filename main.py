7c7fab1bef378b7980c03765e5bfaa16c37c89df
otter502 <115671877+otter502@users.noreply.github.com>
2025-02-10 14:38:12 -0800

    Write pseudocode as comments

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print("hello world")
+
+# MVP needed modules:
+# reading toml
+# reading keyboard
+# running virtual joystick
+
+class KeyMap:
+    def __init__(self, fromKey, toKey) -> None:
+        self.fromKey = fromKey
+        self.toKey = toKey
+
+
+# some method of selecting a toml file
+
+# some function to convert toml file to list of KeyMap(s)
+
+# method which resets keymaps and then sets them based of a list of KeyMap(s)
+
+# something to preform these actions as needed or a loop that calls these methods as needed
+
