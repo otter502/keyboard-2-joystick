4455d97f5a196a0197ef0cba0292171b741bbc60
otter502 <115671877+otter502@users.noreply.github.com>
2025-02-10 23:59:13 -0800

    Add basic keyboard mapper setup

--- main.py
+++ main.py
@@ -10,3 +10,16 @@
 
 # something to preform these actions as needed or a loop that calls these methods as needed
 
+from tkinter  import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+from config_reader import getToml
+from config_reader import getKeyboardMap
+import hotkeyer
+
+
+filename = askopenfilename(filetypes=[("keyboard config", ".toml")]) # show an "Open" dialog box and return the path to the selected file
+
+hotkeyer.mapKeys(getKeyboardMap(getToml(filename)))
+
+while True:
+    pass
