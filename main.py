1f2c19ca8b124939243845a929363ab1fac2a18b
otter502 <115671877+otter502@users.noreply.github.com>
2025-02-14 10:13:03 -0800

    Remove old comments from main.py

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# MVP needed modules:
-# reading toml
-# reading keyboard
-
-# some method of selecting a toml file
-
-# some function to convert toml file to list of KeyMap(s)
-
-# method which resets keymaps and then sets them based of a list of KeyMap(s)
-
-# something to preform these actions as needed or a loop that calls these methods as needed
-
-from tkinter  import Tk     # from tkinter import Tk for Python 3.x
+from tkinter  import Tk
 from tkinter.filedialog import askopenfilename
 from config_reader import getToml
 from config_reader import getKeyboardMap
